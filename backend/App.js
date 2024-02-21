efee89dec0cc815edd0096712ee55eea96078601
sachin333026 <160223294+sachin333026@users.noreply.github.com>
2024-02-21 17:58:05 +0530

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -14,7 +14,25 @@ const moment = require('moment-timezone');
 const server = http.createServer(app);
 
 // live
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // const io = new Server(server, {
 //     cors: {
 //         origin: "http://colorgame.fairyfloss.in",
